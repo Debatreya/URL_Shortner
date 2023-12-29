3d0e65a21ddfcad42292c4937d4d29eb1ca7b341
Debatreya <debatreyadas@gmail.com>
2023-12-30 02:06:13 +0530

    Backend of URl Shortner

---
+++ controllers/url.js
@@ -0,0 +1,55 @@
+const shortid = require('shortid');
+const URL = require('../models/url')
+
+const handleGenerateShortURL = async(req, res) => {
+    const body = req.body;
+    if(!body.url) return res.status(400).json({ error: "url is required"})
+    console.log(body.url);
+    const shortID = shortid();
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    })
+    return res.json({ id: shortID });
+}
+
+const handleRedirect = async (req, res) => {
+    const shortId = req.params.shortId;
+    try{
+        const entry = await URL.findOneAndUpdate( //need to understand this part
+            {
+                shortId,
+            },
+            {
+                $push: {
+                    visitHistory: {timestamp: Date.now()},
+                },
+            }
+        );
+        res.redirect(entry.redirectURL);
+    }
+    catch(err){
+        res.status(400).json( {msg: "ID not found"} )
+    }
+}
+const handleGetAnalytics = async (req, res) => {
+    const shortId  = req.params.shortId;
+    let result
+    try{
+        result = await URL.findOne({ shortId })
+    }
+    catch(err){
+        res.status(404).json({msg: "ID Not Found"});
+    }
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    })
+}
+
+module.exports = {
+    handleGenerateShortURL,
+    handleRedirect,
+    handleGetAnalytics,
+}
